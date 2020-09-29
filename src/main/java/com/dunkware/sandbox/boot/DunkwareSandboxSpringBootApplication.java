2c7ef6c56a5b5edb8cf858b1674e50b2f3544c8a
“Duncan <dkrebs@krebsnet.com>
2020-09-29 02:58:05 -0600

    first commit

---
+++ src/main/java/com/dunkware/sandbox/boot/DunkwareSandboxSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.dunkware.sandbox.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DunkwareSandboxSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DunkwareSandboxSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dunkware/sandbox/boot/DunkwareSandboxSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dunkware.sandbox.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DunkwareSandboxSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
